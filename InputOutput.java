99d82a8031620c95237aaafb9adbd5e97459a958
mirzayogy <mirza.yogy@gmail.com>
2024-04-22 15:18:50 +0800

    menambahkan Scanner

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class InputOutput{
     public static void main(String[] args){
-        
+        int i = 0;
+        double ipk = 4.0;
+        //NamaClass namaObject = new Constructor()
+        Scanner scanner = new Scanner(System.in);
+        String nama = "Mirza";
+        System.out.println("Nama :" + nama);
+        System.out.print("Input nama baru :");
+        nama = scanner.nextLine();
+        System.out.println("Nama baru:" + nama);
+
     }
 }
